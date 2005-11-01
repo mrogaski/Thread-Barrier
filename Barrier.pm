5ddf933603ddc4977fca7ad7ea7a4a286192494b
wendigo <wendigo>
2005-11-01 18:30:54 +0000

    Switched to a hash-based implementation for clarity. Fixed init() method to check for barrier-release condition after it updates the threshold.

--- Barrier.pm
+++ Barrier.pm
@@ -1,11 +1,11 @@
 ###########################################################################
-# $Id: Barrier.pm,v 1.4 2005/10/31 05:37:30 wendigo Exp $
+# $Id: Barrier.pm,v 1.5 2005/11/01 18:30:54 wendigo Exp $
 ###########################################################################
 #
 # Barrier.pm
 #
-# RCS Revision: $Revision: 1.4 $
-# Date: $Date: 2005/10/31 05:37:30 $
+# RCS Revision: $Revision: 1.5 $
+# Date: $Date: 2005/11/01 18:30:54 $
 #
 # Copyright 2002, 2005 Mark Rogaski, mrogaski@cpan.org
 #
@@ -19,51 +19,120 @@ package Thread::Barrier;
 use 5.008;
 use strict;
 use warnings;
+
 use threads;
 use threads::shared;
+use Carp;
 
 our $VERSION = '0.200';
 
+#
+# new - creates a new Thread::Barrier object
+#
+# Arguments:
+#
+# threshold (opt)
+#   Specifies the required number of threads that 
+#   must block on the barrier before it is released.
+#   Default value is 0.
+# 
+# Returns a Thread::Barrier object on success, dies on failure.
+#
 sub new {
-    my $class = shift;
-    my $val   = shift || 0;
-    my @self : shared;
-    die "invalid argument supplied" if $val =~ /[^0-9]/;
-    @self = ( $val, 0 );
-    bless \@self, $class;
+    my $class       = shift;
+    my $threshold   = shift || 0;
+
+    # quick check for a nonnegative integer
+    confess "invalid argument supplied" if $threshold =~ /[^0-9]/;
+
+    my %self : shared;
+    %self = (
+        threshold   => $threshold,
+        count       => 0
+    );
+
+    bless \%self, $class;
 }
 
+
+#
+# init - set the threshold value for the barrier
+#
+# Arguments:
+#
+# threshold
+#   Specifies the required number of threads that 
+#   must block on the barrier before it is released.
+# 
+# Returns the passed argument.
+#
 sub init {
-    my($self, $val) = @_;
-    die "no argument supplied" unless defined $val;
-    die "invalid argument supplied" if $val =~ /[^0-9]/;
-    lock $self;
-    $self->[0] = $val;
+    my($self, $threshold) = @_;
+
+    # make sure an argument was passed
+    confess "no argument supplied" unless defined $threshold;
+
+    # verify that the argument is a nonnegative integer
+    confess "invalid argument supplied" if $threshold =~ /[^0-9]/;
+
+    {
+        #
+        # This could be called with threads already blocking,
+        # so we'll make sure we do appropriate locking.
+        #
+        lock $self;     
+        $self->{threshold} = $threshold;
+
+        if ($self->{threshold} <= $self->{count}) {
+            # release the barrier if enough threads are blocking
+            $self->{count} = 0;
+            cond_broadcast($self);
+        }
+    }
+
+    return $threshold;
 }
 
+
+#
+# wait - block until a sufficient number of threads have reached the barrier
+#
+# Arguments:
+#
+# none
+#
 sub wait {
     my $self = shift;
     lock $self;
-    $self->[1]++;
+    $self->{count}++;
     my $id = threads->self->tid;
-    if ($self->[0] > $self->[1]) {
-        cond_wait($self) while ($self->[1] && $self->[0] > $self->[1]);
+    if ($self->{threshold} > $self->{count}) {
+        cond_wait($self) while ($self->{count} && 
+                $self->{threshold} > $self->{count});
     } else {
-        $self->[1] = 0;
+        $self->{count} = 0;
         cond_broadcast($self);
     }
 }
 
-sub expected {
+
+#
+# threshold - accessor for debugging purposes
+#
+sub threshold {
     my $self = shift;
     lock $self;
-    return $self->[0];
+    return $self->{threshold};
 }
 
-sub waiting {
+
+#
+# count - accessor for debugging purposes
+#
+sub count {
     my $self = shift;
     lock $self;
-    return $self->[1];
+    return $self->{count};
 }
 
 1;
@@ -109,26 +178,47 @@ If C<NUMBER> is not specified, the threshold is set to 0.
 =item init COUNT
 
 C<init> specifies the threshold count for the barrier, must be zero or a
-positive integer.
+positive integer.  If the value of C<COUNT> is less than or equal to the
+number of threads blocking on the barrier when C<init> is called, the barrier
+is released and reset.
 
 =item wait
 
 C<wait> causes the thread to block until the number of threads blocking on 
-the barrier meets the threshold.
+the barrier meets the threshold.  When the blocked threads are released, the
+barrier is reset to its initial state.
+
+=item threshold
+
+Returns the currently configured threshold.
+
+=item count
+
+Returns the instantaneous count of threads blocking on the barrier.
+
+B<WARNING:  This is an accessor method that is intended for debugging 
+purposes only, the lock on the barrier object is released when the 
+method returns.>  
 
 =back
+
 
 =head1 SEE ALSO
 
 L<perlthrtut>.
 
+
 =head1 AUTHOR
 
 Mark Rogaski, E<lt>mrogaski@cpan.orgE<gt>
 
+If you find this module useful or have any questions, comments, or 
+suggestions please send me an email message.
+
+
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2002 by Mark Rogaski
+Copyright 2002, 2005 by Mark Rogaski.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
@@ -137,6 +227,7 @@ This program is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 or FITNESS FOR A PARTICULAR PURPOSE.  See the README file distributed with
 Perl for further details.
+
 
 =cut
 
